672e7f35d4d2f8a022e2f9ea980516fa065321b1
Nachiket Pardesi <nachiketpardesi47@gmail.com>
2023-12-31 19:23:14 +0530

    Minor modification

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Omnifood landing page
 
 This is a simple landing page for a fictional start-up named "Omnifood".
+
 This was made using HTML, CSS and just a little JavaScript.
 
 [Live Site](https://omnifood-project-one.netlify.app/)
--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
 ///////////////////////////////////////////////////////////
-// TEST
-// console.log("Hello World!!");
-
-///////////////////////////////////////////////////////////
 // Set Current Year
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
@@ -23,8 +19,6 @@ const allLinks = document.querySelectorAll("a:link");
 
 allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
-    // e.preventDefault();
-
     const href = link.getAttribute("href");
 
     // Scroll back to top
@@ -59,7 +53,6 @@ const sectionHeroEl = document.querySelector(".section-hero");
 const obs = new IntersectionObserver(
   function (entries) {
     const ent = entries[0];
-    // console.log(ent);
     if (ent.isIntersecting === false) {
       document.body.classList.add("sticky");
     } else {
@@ -67,7 +60,6 @@ const obs = new IntersectionObserver(
     }
   },
   {
-    //  In the viewport
     root: null,
     threshold: 0,
     rootMargin: "-80px",
@@ -94,56 +86,3 @@ function checkFlexGap() {
   if (!isSupported) document.body.classList.add("no-flexbox-gap");
 }
 checkFlexGap();
-
-// https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
-
-/*
-.no-flexbox-gap .main-nav-list li:not(:last-child) {
-  margin-right: 4.8rem;
-}
-
-.no-flexbox-gap .list-item:not(:last-child) {
-  margin-bottom: 1.6rem;
-}
-
-.no-flexbox-gap .list-icon:not(:last-child) {
-  margin-right: 1.6rem;
-}
-
-.no-flexbox-gap .delivered-faces {
-  margin-right: 1.6rem;
-}
-
-.no-flexbox-gap .meal-attribute:not(:last-child) {
-  margin-bottom: 2rem;
-}
-
-.no-flexbox-gap .meal-icon {
-  margin-right: 1.6rem;
-}
-
-.no-flexbox-gap .footer-row div:not(:last-child) {
-  margin-right: 6.4rem;
-}
-
-.no-flexbox-gap .social-links li:not(:last-child) {
-  margin-right: 2.4rem;
-}
-
-.no-flexbox-gap .footer-nav li:not(:last-child) {
-  margin-bottom: 2.4rem;
-}
-
-@media (max-width: 75em) {
-  .no-flexbox-gap .main-nav-list li:not(:last-child) {
-    margin-right: 3.2rem;
-  }
-}
-
-@media (max-width: 59em) {
-  .no-flexbox-gap .main-nav-list li:not(:last-child) {
-    margin-right: 0;
-    margin-bottom: 4.8rem;
-  }
-}
-*/
